9139e91303171ae2a46b7a57581eb0a181caf45a
RG4116 <rukanca@gmail.com>
2025-03-25 17:46:21 -0400

    Service worker tamamen kaldırıldı

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Stillerinizi burada import edin
-import serviceWorkerRegistration from './serviceWorkerRegistration'; // Service worker kaydı
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// PWA servis worker'ını kaydet
-serviceWorkerRegistration();
